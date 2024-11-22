5df21e766f837c5405b1d81043a14770d5514ed2
JeanEudes-dev <152700827+JeanEudes-dev@users.noreply.github.com>
2024-11-22 02:12:37 +0200

    Remove eslint-plugin-react from eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -8,5 +7,4 @@ export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
